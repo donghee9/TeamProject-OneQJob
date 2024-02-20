4be9c2d22419e62776515a0addf4f9b437b18e8b
donghee9 <seodonghee456@gmail.com>
2024-02-20 12:35:30 +0900

    초기세팅

---
+++ src/main/java/settleup/backend/SettleUpApplication.java
@@ -0,0 +1,13 @@
+package settleup.backend;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SettleUpApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SettleUpApplication.class, args);
+    }
+
+}
---
+++ src/test/java/settleup/backend/SettleUpApplicationTests.java
@@ -0,0 +1,13 @@
+package settleup.backend;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SettleUpApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
